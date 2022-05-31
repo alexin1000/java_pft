ed48e0b2830a596b0120d02a0f5ae15fc9817b9a
alexin1000 <106390700+alexin1000@users.noreply.github.com>
2022-05-31 11:10:23 -0400

    funcs

--- sandbox/src/main/java/ca/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ca/stqa/pft/sandbox/MyFirstProgram.java
@@ -1,7 +1,18 @@
 package ca.stqa.pft.sandbox;
 
-public class  MyFirstProgram {
-    public static void main(String[] args) {
-        System.out.println("Hello, world!!");
-    } 
+public class MyFirstProgram {
+  public static void main(String[] args) {
+    int a = 2;
+    int b = 3;
+    System.out.println("Hello, world!!");
+
+    System.out.println(a + b);
+
+    kvadrat(a);
+    kvadrat(b);
+  }
+
+  public static void kvadrat(int a) {
+    System.out.println("Kvadrat = " + (a * a));
+  }
 }
